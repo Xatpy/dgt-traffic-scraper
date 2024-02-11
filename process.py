1962fa8677161e394bd5d3cd6aa2a6680343d98c
Jaime <jaime.chapinal@gmail.com>
2024-02-11 12:32:54 +0100

    Resolving conflict

--- process.py
+++ process.py
@@ -12,6 +12,18 @@ def parse_file_name(file_name):
 
 def parse_csv_content(content):
     desired_columns = ['codEle', 'alias', 'provincia', 'poblacion']
+
+    parsed_data = []
+    reader = csv.DictReader(content.splitlines())
+    
+    for row in reader:
+        parsed_row = {col: row[col] for col in desired_columns}
+        parsed_data.append(parsed_row)
+
+    return parsed_data
+
+def parse_csv_content_graphext(content):
+    desired_columns = ['codEle', 'alias', 'autonomia', 'provincia', 'poblacion', 'fecha', 'hora', 'sentido', 'descripcion', 'fechaFin', 'lng', 'lat', 'pkIni', 'pkFinal', 'causa', 'carretera', 'estado', 'tipo' ]
     
     parsed_data = []
     reader = csv.DictReader(content.splitlines())
@@ -40,6 +52,17 @@ def read_and_store_content(file_path, data_structure):
 
     data_structure[date_key][hour_key].extend(parsed_data)
 
+def read_and_store_content_graphext(file_path, data_structure_graphext):
+    with open(file_path, 'r') as file:
+        content = file.read()
+
+    parsed_data_graphext = parse_csv_content_graphext(content)
+
+    # Filter out elements from the new list that are already present in the original list
+    filtered_new_list = [item for item in parsed_data_graphext if item['codEle'] not in {obj['codEle'] for obj in data_structure_graphext}]
+
+    data_structure_graphext.extend(filtered_new_list)
+
 
 def process_files(directory_path):
     data_structure = {}
@@ -52,17 +75,31 @@ def process_files(directory_path):
 
     return data_structure
 
-# Get the directory path relative to the script
+def process_files_graphext(directory_path):
+    data_structure_graphext = []
+
+    for file_name in os.listdir(directory_path):
+        file_path = os.path.join(directory_path, file_name)
+        
+        if os.path.isfile(file_path) and file_name.endswith('_traffic_incidents.csv'):
+            read_and_store_content_graphext(file_path, data_structure_graphext)
+
+    return data_structure_graphext
+
 script_dir = os.path.dirname(os.path.abspath(__file__))
-relative_directory_path = './out'
+relative_directory_path = './out/all'
 directory_path = os.path.join(script_dir, relative_directory_path)
 
 result_data_structure = process_files(directory_path)
-
 json_output_file = 'out/processed/processed_data.json'
 json_output = json.dumps(result_data_structure, indent=2, sort_keys=True)
-
 with open(json_output_file, 'w') as json_file:
     json_file.write(json_output)
+
+result_data_structure_graphext = process_files_graphext(directory_path)
+json_output_file_graphext = 'out/processed/processed_data_graphext.json'
+json_output_graphext = json.dumps(result_data_structure_graphext, indent=2, sort_keys=True)
+with open(json_output_file_graphext, 'w') as json_file:
+    json_file.write(json_output_graphext)
 
 print(result_data_structure)
